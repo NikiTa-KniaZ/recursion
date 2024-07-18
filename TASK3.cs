6b3ada91e8e83c59465c984d57812ac9a66325d3
Nikita <nikita27.1027@gmail.com>
2024-07-18 16:48:33 +0200

    commit 2

--- TASK3.cs
+++ TASK3.cs
@@ -13,3 +13,27 @@
             PrintArrayReverse(arr, index - 1); 
         } 
     } 
+
+{ 
+Console.WriteLine(""); 
+Console.Write("Введите неотрицательное число m: "); 
+int m = int.Parse(Console.ReadLine()); 
+ 
+Console.Write("Введите неотрицательное число n: "); 
+int n = int.Parse(Console.ReadLine()); 
+ 
+int result = AckermannFunction(m, n); 
+Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно: {result}"); 
+    
+ 
+    int AckermannFunction(int m, int n) 
+    { 
+        if (m == 0) 
+            return n + 1; 
+        else if (n == 0) 
+            return AckermannFunction(m - 1, 1); 
+        else 
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1)); 
+    } 
+}
+
