6bbc0de59cb8572628af427057f58925622e1a59
Nikita <nikita27.1027@gmail.com>
2024-07-18 16:41:58 +0200

    first commit

---
+++ TASK3.cs
@@ -0,0 +1,15 @@
+﻿{ 
+        int[] array = { 1, 12, 54, 42, 65, 18, 256 }; 
+ 
+        Console.WriteLine("Элементы массива, начиная с конца:"); 
+        PrintArrayReverse(array, array.Length - 1); 
+    } 
+ 
+    void PrintArrayReverse(int[] arr, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayReverse(arr, index - 1); 
+        } 
+    } 
